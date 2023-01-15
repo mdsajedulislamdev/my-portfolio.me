2022cf1ecfe3c71bddd1638bf8bc6df396f5cdf5
codinghuntsman <sajedulislam.cse@gmail.com>
2023-01-15 17:25:15 +0600

    mailto link added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,7 +6,9 @@ const Contact = () => {
     <>
       <div className={style.ContactMainDiv} id="contact">
         <h2 className={style.ContactText}>Contact Me</h2>
-        <h2 className={style.ContactEmail}>Email: sajedulislam.cse@gmail.com</h2>
+        <a className={style.ContactEmail} href="mailto:sajedulislam.cse@gmail.com">
+          Email: sajedulislam.cse@gmail.com
+        </a>
         <div className={style.ContactNumberDiv}>
           <h2>MObile: +880 17373 22635</h2>
           <h2>MObile: +880 1954 145647</h2>
